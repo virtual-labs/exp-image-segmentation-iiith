b57c27b112aee470b16070139a50aa819c12b8e6
Gaurav <gaurav.behera@research.iiit.ac.in>
2024-06-27 01:03:46 +0530

    Upgraded code
    Upgraded to PHP 8.2 using rector.
    Upgraded simulation opencv code

--- experiment/assignment/segment_x.php
+++ experiment/assignment/segment_x.php
@@ -628,10 +628,10 @@ $("#sliderContent").replaceWith('      <div id="sliderContent" class="ui-corner-
 <div id="nextBox">
 </div>
  <div id="navig">
-      <?if (!isset($_GET["assess"]) && $_SESSION["piping"]!=99)  { ?> <button1>Select Image</button1>   <? } ?>
+      <?php if (!isset($_GET["assess"]) && $_SESSION["piping"]!=99)  { ?> <button1>Select Image</button1>   <?php } ?>
       <button4>Reset</button4>
       <button2>Run</button2>
-	 <?if($_SESSION["piping"]==99) { ?> <button7>Return to Piping</button7> <?php } ?>
+	 <?php if($_SESSION["piping"]==99) { ?> <button7>Return to Piping</button7> <?php } ?>
    <br/><br/> </div>
 
 
