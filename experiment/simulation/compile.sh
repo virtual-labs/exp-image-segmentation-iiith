6d94906dca2fe0bd9309cbe050bb3fd4b588ef05
mittalshivam2709 <118185002+mittalshivam2709@users.noreply.github.com>
2024-12-30 20:56:48 +0530

    php to js converted code

--- experiment/simulation/compile.sh
+++
@@ -1,9 +0,0 @@
-export OPENCV_FLAGS=$(pkg-config --cflags --libs opencv4)
-
-g++ codes/im_segment.cpp $OPENCV_FLAGS -o execs/segment.out
-g++ codes/im_colour.cpp $OPENCV_FLAGS -o execs/colour.out 
-g++ codes/im_hist.cpp $OPENCV_FLAGS -o execs/hist.out 
-
-cp execs/segment.out ../assignment/execs/segment.out
-cp execs/colour.out ../assignment/execs/colour.out
-cp execs/hist.out ../assignment/execs/hist.out
